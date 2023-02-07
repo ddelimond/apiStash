d24a31c675beabae69fcdb60ab1b0bb33ed3190c
Darren Delimond <darrendelimond@gmail.com>
2023-02-07 07:07:49 -0500

    =test

---
+++ index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.port || 8000
+
+console.log(process.env.connectionString)
+
+mongoose.connect(process.env.connectionString)
+    .then(() => {
+        console.log('connected to Db Successfully')
+    })
+    .catch((err) => {
+        console.log(`failed to connect to Db error:${err}`)
+    })
+
+
+app.listen(port, () => {
+    console.log('Backend is working!')
+})
